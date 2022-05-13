e1a490c5b1bfa09c02df15b5c3740638140f91e2
PRESSO <user1205@naver.com>
2022-05-13 15:01:56 +0900

    Separate HEADER

---
+++ data.py
@@ -0,0 +1,17 @@
+HEADER = {
+    'Accept': 'text/html, /; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6,ja;q=0.5',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Host': 'info.hansung.ac.kr',
+    'Referer': 'https://info.hansung.ac.kr/jsp/sugang/h_sugang_sincheong_main.jsp',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
--- util.py
+++ util.py
@@ -1,24 +1,3 @@
-# res = {
-#     {
-#         "semester": "",
-#         "register_credits": "",
-#         "earned_credits": "",
-#         "total_score": "",
-#         "average_credits": "",
-#         "percentile": "",
-#         "subject": {
-#             {
-#                 "classification": "",
-#                 "name": "",
-#                 "code": "",
-#                 "credits": "",
-#                 "grade": "",
-#                 "track": ""
-#             }
-#         }
-#     }
-# }
-
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
@@ -27,23 +6,9 @@ from bs4 import BeautifulSoup as bs4
 import re
 import json
 
-header = {
-    'Accept': 'text/html, /; q=0.01',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6,ja;q=0.5',
-    'Connection': 'keep-alive',
-    'DNT': '1',
-    'Host': 'info.hansung.ac.kr',
-    'Referer': 'https://info.hansung.ac.kr/jsp/sugang/h_sugang_sincheong_main.jsp',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
-}
+from data import HEADER
+
+header = HEADER
 
 def parse(session, address):
     grade_page = session.get(address, headers=header).text
