335b87aa75bc08a7d6c380a2c846454924c5dc93
PRESSO <caff1nepill@gmail.com>
2023-01-24 18:19:15 +0900

    update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-bind = 'unix:/hansung-grade-backend/gunicorn.sock'
+bind = 'unix:/run/gunicorn.sock'
 
 # Worker Options
 workers = cpu_count() * 2 + 1
